66f8b4439d158a6ec2c529ff7c27bbe9b79a9df7
Petr Vosoust <petr.vosoust.ml@email.cz>
2025-02-16 02:55:23 +0100

    More stuff
    - enum extensions
    - global key hooking

---
+++ CsharpLib/Utils/Extends/Enums.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Vosiz.Commons;
+
+namespace Vosiz.Utils.Extends
+{
+    public static class Enums
+    {
+        public static string GetDescription(this Enum value)
+        {
+            Assert.OnNull(value);
+
+            try
+            {
+                FieldInfo field = value.GetType().GetField(value.ToString());
+
+                if (field != null)
+                {
+                    DescriptionAttribute attribute = field.GetCustomAttribute<DescriptionAttribute>();
+
+                    if (attribute != null)
+                    {
+                        return attribute.Description;
+                    }
+                }
+
+                // default
+                return value.ToString();
+            }
+            catch (Exception exc)
+            {
+
+                throw exc;
+            }
+
+        }
+
+        public static IEnumerable<T> GetAll<T>(this T _) where T : Enum
+        {
+            return (T[])Enum.GetValues(typeof(T));
+        }
+    }
+}
---
+++ CsharpLib/Utils/Windows/GlobalKeyHook.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Vosiz.Utils.Windows
+{
+    public class GlobalKeyHook
+    {
+
+        private const int WH_KEYBOARD_LL = 13;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        public bool Enabled = false;
+
+        private IntPtr HookUd = IntPtr.Zero;
+        private LowLevelKeyboardProc Process;
+        private Action Callback;
+        private Keys[] Modifiers;
+        private Keys[] Keys;
+
+        public GlobalKeyHook(Action callback, Keys[] keys, Keys[] modifiers = null)
+        {
+            Process = HookCallback;
+            Callback = callback;
+            HookUd = SetHook(Process);
+            Enabled = true;
+            Modifiers = modifiers;
+            Keys = keys;
+        }
+
+        public void Dispose()
+        {
+            UnhookWindowsHookEx(HookUd);
+        }
+
+        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = System.Diagnostics.Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+
+            if (nCode >= 0 && Enabled)
+            {
+                // read
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                var mods_pressed = true;
+                if (Modifiers.Length > 0)
+                {
+
+                    foreach (var mod in Modifiers)
+                    {
+
+                        if ((Control.ModifierKeys & mod) == 0)
+                        {
+                            mods_pressed = false;
+                        }
+                    }
+                }
+
+                var key_pressed = false;
+                if (Modifiers.Length > 0)
+                {
+
+                    foreach (var k in Keys)
+                    {
+
+                        if ((Keys)vkCode == k)
+                        {
+                            key_pressed = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (key_pressed && mods_pressed)
+                {
+                    Callback?.Invoke();
+                }
+
+            }
+
+            return CallNextHookEx(HookUd, nCode, wParam, lParam);
+        }
+
+
+    }
+}
